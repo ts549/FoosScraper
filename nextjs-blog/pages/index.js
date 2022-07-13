a3edf4ad0bff18528b19cc5b265756ab58debc2d
ts549 <sun955@purdue.edu>
2022-07-13 17:57:41 -0400

    added search bar foo

--- nextjs-blog/pages/index.js
+++ nextjs-blog/pages/index.js
@@ -1,4 +1,13 @@
 import Head from 'next/head'
+import SearchBar from "../components/SearchBar"
+import algoliasearch from "algoliasearch/lite";
+import { InstantSearch, SearchBox, Hits } 
+    from "react-instantsearch-dom";
+
+const searchClient = algoliasearch(
+  "XJKWXUI2BM",
+  "595a1f63030718fe0b164a2c2fbfe686",
+);
 
 export default function Home() {
   return (
@@ -15,33 +24,16 @@ export default function Home() {
         </h1>
 
         <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+          <InstantSearch 
+            searchClient={searchClient} 
+            indexName="gfg_dev">
+      
+            {/* Adding Search Box */}
+            <SearchBar/>
+      
+            {/* Adding Data */}
+            <Hits />
+          </InstantSearch>
         </div>
       </main>
 
