7a6add2b350251f07df3a71fe9f72cd50525f803
Edecker123 <ethan@Ethans-MacBook-Pro-2.local>
2022-07-14 15:06:56 -0400

    tao lick dingaling while i like tianlang

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const cheerio = require('cheerio')
 const app = express()
 const PORT=8000
 
-const search='suckmydikcunt'
+const search='c++'
 
 const url='https://stackoverflow.com/search?q=' + search + '&s=22cf9f4c-05b1-47ff-9e69-b5349ba052d0'
 
@@ -13,11 +13,18 @@ axios(url)
     .then(response=>{
         const html=response.data
         const $ = cheerio.load(html)
-        $('.s-topbar--searchbar--input-group', html).each(function() {
+        const articles=[]
+        $('.s-link',html).each(function () {
             const title = $(this).text()
-
+            const url2 = $(this).attr('href')
+            
+            articles.push(
+                title,
+                'https://stackoverflow.com' + url2)
         })
-    })
+    console.log(articles[5])
+    
+})
 
 
 app.listen(PORT, () => console.log(url))
